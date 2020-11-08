b71063d6b43f2dd5bbcf903ba564204be8e9120b
Thomas Kuenneth <thomas.kuenneth@mathema.de>
2020-11-08 14:31:09 +0100

    Project updates

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module tkmactuning {
+    requires javafx.base;
+    requires javafx.controls;
+    requires java.logging;
+    requires java.prefs;
+    requires json;
+    opens com.thomaskuenneth.tkmactuning;
+}
